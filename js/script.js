1b6b11ff06a03b93dad75ae0e20a7a221c6e916d
stoe <vladstoe@yahoo.com>
2023-02-09 17:58:37 +0200

    git commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-// alert("Hello there");
+'use strict'
+
+// navbar variables
+
+const navOpenBtn = document.querySelector("[data-menu-open-btn]");
+const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+
+if (!navOpenBtn || !navCloseBtn || !navbar || !overlay) {
+    console.error("One or more elements were not found");
+  }
+
+for(let i=0; i<navElemArr.length; i++){
+    navElemArr[i].addEventListener("click", function() {     //function e un fel de -> (argument pt eventListener)
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+        document.body.classList.toggle("active");
+    });
+}  //le da proprietatea 'active' atunci cand sunt clicked
+
+
+// header sticky
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function(){
+    this.window.scrollY >= 10? header.classList.add("active") : header.classList.remove("active");
+});
